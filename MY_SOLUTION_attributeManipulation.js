c803b569f631cb19513ba5ef5c834d737cc3b3d2
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-09-06 00:23:03 +0600

    done

---
+++ MY_SOLUTION_attributeManipulation.js
@@ -0,0 +1,24 @@
+{
+  /* 
+
+<a name="list">the list</a>
+<ul>
+  <li><a href="http://google.com">http://google.com</a></li>
+  <li><a href="/tutorial">/tutorial.html</a></li>
+  <li><a href="local/path">local/path</a></li>
+  <li><a href="ftp://ftp.com/my.zip">ftp://ftp.com/my.zip</a></li>
+  <li><a href="http://nodejs.org">http://nodejs.org</a></li>
+  <li><a href="http://internal.com/test">http://internal.com/test</a></li>
+</ul> 
+
+*/
+}
+
+document.querySelectorAll("li")[0].style.backgroundColor = "green";
+document.querySelectorAll("li")[1].style.backgroundColor = "Tan";
+document.querySelectorAll("li")[2].style.backgroundColor = "yellow";
+document.querySelectorAll("li")[3].style.backgroundColor = "blue";
+document.querySelectorAll("li")[4].style.backgroundColor = "red";
+document.querySelectorAll("li")[5].style.backgroundColor = "pink";
+
+console.log("working !!");
