155112ec6c15cd67ff61fe14dc6ca51285bc1b67
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-09-06 09:59:54 +0600

    colordedBox done

---
+++ DOM-JS/Task/MY_SOLUTION_coloredBox.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("tr")[0].bgColor = "green";
+document.querySelectorAll("tr")[1].bgColor = "red";
+document.querySelectorAll("tr")[2].bgColor = "blue";
+document.querySelectorAll("tr")[3].bgColor = "pink";
+document.querySelectorAll("tr")[4].bgColor = "yellow";
+document.querySelectorAll("tr")[5].bgColor = "tan";
