81b2c444d586f7e82a43d542805126e84273690a
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-09-07 13:14:14 +0600

    Generate-RGB-Color-By-Clicking.js completed

--- DOM-JS/Task/Generate-RGB-Color-By-Clicking/Generate-RGB-Color-By-Clicking.js
+++ DOM-JS/Task/Generate-RGB-Color-By-Clicking/Generate-RGB-Color-By-Clicking.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("change-btn");
 btn.addEventListener("click", function () {
-  console.log("Clicked");
+  // console.log("Clicked");
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
