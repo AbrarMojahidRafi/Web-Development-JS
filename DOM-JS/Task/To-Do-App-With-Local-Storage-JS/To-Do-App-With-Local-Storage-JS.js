f64e791cc692ea52cb161f0972619273f1fdb1fb
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-09-23 23:07:35 +0600

    To-Do-App-With-Local-Storage-JS project first part done

---
+++ DOM-JS/Task/To-Do-App-With-Local-Storage-JS/To-Do-App-With-Local-Storage-JS.js
@@ -0,0 +1,13 @@
+const userInput = document.getElementsByTagName("input")[0];
+const addButton = document.getElementById("push");
+
+const tasks = document.getElementById("tasks");
+
+// console.log(userInput, addButton);
+
+addButton.addEventListener("click", function () {
+  const new_div = document.createElement("div");
+  new_div.textContent = userInput.value;
+  tasks.appendChild(new_div);
+  console.log("clicked");
+});
